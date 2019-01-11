4c014763f66d532070695382b7586f9a4a5cc7a4
Allen, Timothy <tallen@wharton.upenn.edu>
2019-01-11 13:53:38 +0000

    Include JavaScript payload for ReactJS meeting guide in template tag.

--- meeting_guide/urls.py
+++ meeting_guide/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import MeetingsReactJSView, MeetingsAPIView
 
 
+app_name = "meeting-guide"
+
 urlpatterns = [
     path('', MeetingsReactJSView.as_view(), name="react"),
     path('api/', MeetingsAPIView.as_view(), name="api"),
