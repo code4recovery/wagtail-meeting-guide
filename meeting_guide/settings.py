e3a78eebb72d6ebc6a63de23ca5129848d86d6cb
Allen, Timothy <tallen@wharton.upenn.edu>
2019-03-13 17:40:20 +0000

    Add ability to add CSS styles.

--- meeting_guide/settings.py
+++ meeting_guide/settings.py
@@ -12,12 +12,63 @@ def get_print_options():
         {
             'page-width': '100mm',
             'page-height': '120mm',
+
             'margin-top': '10mm',
             'margin-right': '10mm',
             'margin-bottom': '10mm',
             'margin-left': '10mm',
+
             'header-left': '[section]: [subsection]',
+
             'encoding': "UTF-8",
-            'no-outline': None
+            'no-outline': None,
         },
+    )
+
+
+def get_print_styles():
+    """
+    Default options for PDF styling.
+    """
+
+    return getattr(
+        settings,
+        "WAGTAIL_MEETING_GUIDE_PRINT_STYLES",
+        """
+html, td {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 9px;
+  -webkit-text-size-adjust:100%;
+  -ms-text-size-adjust:100%;
+  color: red;
+}
+
+body {
+  margin:0;
+}
+
+h1, .h1 {
+  font-size: 24px;
+}
+
+h2, .h2 {
+  font-size: 18px;
+}
+
+h3, .h3 {
+  font-size: 14px;
+}
+
+h4, .h4 {
+  font-size: 12px;
+}
+
+h5, .h5 {
+  font-size: 11px;
+}
+
+h6, .h6 {
+  font-size: 9px;
+}
+        """,
     )
