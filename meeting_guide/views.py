3fc4759260426776b230f8cc84e383d352ec10ed
Allen, Timothy <tallen@wharton.upenn.edu>
2020-02-01 15:37:32 -0500

    Remove formatted start/end time; they get build in JS.

--- meeting_guide/views.py
+++ meeting_guide/views.py
@@ -213,8 +213,6 @@ class MeetingsAPIView(MeetingsBaseView):
                     "url": f"{url}{meeting.url_path}",
                     "time": f"{meeting.start_time:%H:%M}",
                     "end_time": f"{meeting.end_time:%H:%M}",
-                    # "formatted_time": f"{meeting.start_time:%H:%M %P}",
-                    # "formatted_end_time": f"{meeting.end_time:%H:%M %P}",
                     "distance": "",
                     "day": str(meeting.day_of_week),
                     "types": list(
